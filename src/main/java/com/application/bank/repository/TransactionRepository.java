276cac88b6bee3708842cc52f2565161ceb4b925
Emmanuel Gabriel <degabriels03@gmail.com>
2026-09-14 14:09:15 +0530

    transaction repository

---
+++ src/main/java/com/application/bank/repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package com.application.bank.repository;
+
+import com.application.bank.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    Page<Transaction> findByAccount_AccountNumber(String accountNumber, Pageable pageable);
+}
